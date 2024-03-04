405ea9a95e433449ebca903feaa7462287bdefd9
Mihaly Barasz <klao@nilcons.com>
2024-03-04 21:25:09 +0100

    Start with better examples

--- src/code.ts
+++ src/code.ts
@@ -9,15 +9,40 @@ import { json } from "@codemirror/lang-json";
 const yamlElem = document.getElementById("ed-yaml")!;
 const jsonElem = document.getElementById("ed-json")!;
 
+////////////////////////////////////////////////////////////////////////////////
+// Examples:
+const yamlExample = `---
+# Comment
+map:
+  key: value
+  one: 1
+  true: true
+  foo: bar
+sequence:
+  - apple
+  - orange
+  - 2.5
+`;
+const jsonExample = `{
+  "map": {
+    "key": "value",
+    "one": 1,
+    "true": true,
+    "foo": "bar"
+  },
+  "sequence": ["apple", "orange", 2.5]
+}`;
+
+////////////////////////////////////////////////////////////////////////////////
 const extensions = [basicSetup, keymap.of([indentWithTab])];
 const yamlView = new EditorView({
-    doc: "Here be yaml",
+    doc: yamlExample,
     extensions: [extensions, yaml()],
     parent: yamlElem,
 });
 
 const jsonView = new EditorView({
-    doc: "Here be json",
+    doc: jsonExample,
     extensions: [extensions, json()],
     parent: jsonElem,
 });
