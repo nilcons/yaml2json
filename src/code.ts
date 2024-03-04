8cf7b093bf17446d95e30b3af37e146810f00033
Mihaly Barasz <klao@nilcons.com>
2024-03-04 21:04:00 +0100

    Support tab indentation

--- src/code.ts
+++ src/code.ts
@@ -1,5 +1,6 @@
 import { basicSetup } from "codemirror";
-import { EditorView } from "@codemirror/view";
+import { EditorView, keymap } from "@codemirror/view";
+import { indentWithTab } from "@codemirror/commands";
 import { yaml } from "@codemirror/lang-yaml";
 import { json } from "@codemirror/lang-json";
 
@@ -8,14 +9,15 @@ import { json } from "@codemirror/lang-json";
 const yamlElem = document.getElementById("ed-yaml")!;
 const jsonElem = document.getElementById("ed-json")!;
 
+const extensions = [basicSetup, keymap.of([indentWithTab])];
 const yamlView = new EditorView({
     doc: "Here be yaml",
-    extensions: [basicSetup, yaml()],
+    extensions: [extensions, yaml()],
     parent: yamlElem,
 });
 
 const jsonView = new EditorView({
     doc: "Here be json",
-    extensions: [basicSetup, json()],
+    extensions: [extensions, json()],
     parent: jsonElem,
 });
