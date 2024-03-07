a4cb4e3e723fd9b13b0df87cb227201c71bdab44
Mihaly Barasz <klao@nilcons.com>
2024-03-07 09:58:20 +0100

    Unify the two sides

--- src/code.ts
+++ src/code.ts
@@ -4,9 +4,7 @@ import { yaml } from "@codemirror/lang-yaml";
 import { json } from "@codemirror/lang-json";
 import { ourSetup } from "./cmsetup";
 
-import { parse, stringify } from "yaml";
-let yamlVersion: "1.1" | "1.2" = "1.1";
-const yparse = (s: string) => parse(s, { version: yamlVersion });
+import * as YAML from "yaml";
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -16,6 +14,66 @@ const messageElem = document.getElementById("message")!;
 const messageTextElem = document.getElementById("message-text")!;
 const closeMessageElem = document.getElementById("close-message")!;
 const yamlVersionElem = document.getElementById("yaml-version")! as HTMLSelectElement;
+
+////////////////////////////////////////////////////////////////////////////////
+// Parsing
+
+type ParseResult = {
+    value: any;
+    errorMessage?: string;
+    errorLine?: number;
+}
+const lineRegex = /line (\d+)/;
+
+let yamlVersion: "1.1" | "1.2" = "1.1";
+function yparse(s: string): ParseResult {
+    try {
+        return { value: YAML.parse(s, { version: yamlVersion }) };
+    }
+    catch (e) {
+        const errorMessage = "" + e;
+        const m = lineRegex.exec(errorMessage);
+        let errorLine: number | undefined;
+        if (m) {
+            errorLine = parseInt(m[1]);
+        }
+        return { value: undefined, errorMessage, errorLine };
+    }
+}
+
+function jparse(s: string): ParseResult {
+    try {
+        return { value: JSON.parse(s) };
+    }
+    catch (e) {
+        const errorMessage = "JSON: " + e;
+        const m = lineRegex.exec(errorMessage);
+        let errorLine: number | undefined;
+        if (m) {
+            errorLine = parseInt(m[1]);
+        }
+        return { value: undefined, errorMessage, errorLine };
+    }
+}
+
+function parse(yamlSide: boolean, s: string): ParseResult {
+    return yamlSide ? yparse(s) : jparse(s);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Format
+
+function yformat(value: any): string {
+    return YAML.stringify(value, { version: yamlVersion, directives: true });
+}
+
+function jformat(value: any): string {
+    return JSON.stringify(value, null, 2);
+}
+
+function format(yamlSide: boolean, value: any): string {
+    return yamlSide ? jformat(value) : yformat(value);
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 // Examples:
@@ -33,68 +91,44 @@ sequence: &copy
 repeated:
   sequence: *copy
 `;
-const initialValue = yparse(yamlExample);
-const jsonExample = JSON.stringify(initialValue, null, 2);
+const initialValue = yparse(yamlExample).value;
+const jsonExample = format(true, initialValue);
 
 let currentValue: string | undefined = JSON.stringify(initialValue);
 
 ////////////////////////////////////////////////////////////////////////////////
 
-const yamlUpdate = (yamlInput: string) => {
-    try {
-        const value = yparse(yamlInput);
-        const s = JSON.stringify(value);
-        if (s === currentValue) return;
-        currentValue = s;
-        jsonView.dispatch({
-            changes: {
-                from: 0,
-                to: jsonView.state.doc.length,
-                insert: JSON.stringify(value, null, 2),
-            },
-        });
-        closeMessage();
-    } catch (e) {
+const sideUpdate = (yamlSide: boolean, input: string) => {
+    const r = parse(yamlSide, input);
+    if (r.errorMessage) {
+        showMessage(r.errorMessage);
         currentValue = undefined;
-        showMessage("" + e);
+        // console.log(r.errorLine);
+        return;
     }
+    const s = JSON.stringify(r.value);
+    if (s === currentValue) return;
+    currentValue = s;
+    const otherView = yamlSide ? jsonView : yamlView;
+    otherView.dispatch({
+        changes: {
+            from: 0,
+            to: otherView.state.doc.length,
+            insert: format(yamlSide, r.value),
+        },
+    });
+    closeMessage();
 };
-
-const yamlPlugin = ViewPlugin.fromClass(class {
-    constructor(_view: EditorView) { }
-
-    update(update: ViewUpdate) {
-        if (!update.docChanged) return;
-        if (!update.transactions.some(tr => tr.annotation(Transaction.userEvent))) return;
-        yamlUpdate(update.view.state.doc.toString());
-    }
-});
 
 ////////////////////////////////////////////////////////////////////////////////
 
-const jsonPlugin = ViewPlugin.fromClass(class {
+const sidePlugin = (yamlSide: boolean) => ViewPlugin.fromClass(class {
     constructor(_view: EditorView) { }
 
     update(update: ViewUpdate) {
         if (!update.docChanged) return;
         if (!update.transactions.some(tr => tr.annotation(Transaction.userEvent))) return;
-        try {
-            const value = JSON.parse(update.view.state.doc.toString());
-            const s = JSON.stringify(value);
-            if (s === currentValue) return;
-            currentValue = s;
-            yamlView.dispatch({
-                changes: {
-                    from: 0,
-                    to: yamlView.state.doc.length,
-                    insert: "---\n" + stringify(value),
-                },
-            });
-            closeMessage();
-        } catch (e) {
-            currentValue = undefined;
-            showMessage("JSON: " + e);
-        }
+        sideUpdate(yamlSide, update.view.state.doc.toString());
     }
 });
 
@@ -103,17 +137,17 @@ const jsonPlugin = ViewPlugin.fromClass(class {
 const extensions = [ourSetup];
 const yamlView = new EditorView({
     doc: yamlExample,
-    extensions: [extensions, yaml(), yamlPlugin],
+    extensions: [extensions, yaml(), sidePlugin(true)],
     parent: yamlElem,
 });
 
 const jsonView = new EditorView({
     doc: jsonExample,
-    extensions: [extensions, json(), jsonPlugin],
+    extensions: [extensions, json(), sidePlugin(false)],
     parent: jsonElem,
 });
 
-window.addEventListener('keydown', function(event: KeyboardEvent) {
+window.addEventListener('keydown', function (event: KeyboardEvent) {
     if (event.key === 'F1' || event.code === 'F1') {
         event.preventDefault();
         yamlView.focus();
@@ -139,7 +173,7 @@ closeMessageElem.addEventListener("click", closeMessage);
 
 yamlVersionElem.addEventListener("change", () => {
     yamlVersion = yamlVersionElem.value as "1.1" | "1.2";
-    yamlUpdate(yamlView.state.doc.toString());
+    sideUpdate(true, yamlView.state.doc.toString());
 });
 
 ////////////////////////////////////////////////////////////////////////////////
